ec4fdb6d6c6d01c091b9670b7e583c243789bb73
Michael Rojas <mikerojas@google.com>
2016-11-27 09:38:03 -0500

    Fix BasicDivisionMethod, Refactor BasicDivisionMethod

--- src/div/basicDivisionMethod.ts
+++ src/div/basicDivisionMethod.ts
@@ -1,5 +1,4 @@
 import {compare, zero} from '../util';
-//import BasicAdditionMethod from '../add/basicAdditionMethod';
 import BasicSubtractionMethod from '../sub/basicSubtractionMethod';
 
 /*
@@ -16,13 +15,11 @@ export default function BasicDivisionMethod(A: number[], lenA: number, B: number
 //INPUT: positive integers x = (xn ··· x1x0)b, y = (yt ··· y1y0)b with n ≥ t ≥ 1, yt != 0.
 //OUTPUT: the quotient q = (qn−t ··· q1q0)b and remainder r = (rt ··· r1r0)b such that x = qy + r, 0 ≤ r < y.
 function divide(X: number[], maxX: number, Y: number[], maxY: number, base: number): [number[], number[], number, number] {
-  const n: number = maxX - 1;
-  const t: number = maxY - 1;
 
   //1. Initialize the quotient
-  const nMt: number = n - t;
+  const nMt: number = maxX - maxY;
   const Q: number[] = new Array(nMt + 1);
-  zero(Q, 0, nMt);
+  zero(Q, 0, nMt + 1);
 
   //2. While X ≥ Yb^(n−t)
   while(compare(X, nMt, maxX, Y, 0, maxY) >= 0){
@@ -35,57 +32,62 @@ function divide(X: number[], maxX: number, Y: number[], maxY: number, base: numb
   }
 
   //3. For i from n down to (t + 1)
-  for(let i: number = n; i > t; --i){
+  const QY: number[] = new Array(maxY + 1);
+  for(let t: number = maxY - 1, i: number = t + nMt; i > t; --i){
+    const iMt: number = i-t-1;
 
     //3.1 If x[i] = y[t] then set q[i−t−1] ← b − 1; otherwise set q[i−t−1] ← (x[i]b + x[i−1]) / y[t]
-    Q[i-t-1] = (X[i] === Y[t]) ? base - 1 : 0 | ((X[i]*base + X[i-1]) / Y[t]);
+    //3.2 While q[i−t−1] * (y[t]b + y[t−1]) > x[i]b^2 + x[i−1]b + x[i−2]
+    Q[iMt] = divThreeHalvesByTwo(X[i], X[i-1], X[i-2], Y[t], Y[t-1], base);
+
+    /*
+    //3.1 If x[i] = y[t] then set q[i−t−1] ← b − 1; otherwise set q[i−t−1] ← (x[i]b + x[i−1]) / y[t]
+    Q[iMt] = (X[i] === Y[t]) ? base - 1 : 0 | ((X[i]*base + X[i-1]) / Y[t]);
 
     //pre-3.2 q[i-t-1] * (y[t]b + y[t-1])
-    const QY: number[] = new Array(3);
-    let maxQY: number = mul(Y, t-1, 2, Q[i-t-1], QY, base);
+    let maxQY: number = mul(Y, t-1, 2, Q[iMt], QY, base);
+    zero(QY, maxQY, 3);
 
     //3.2 While q[i−t−1] * (y[t]b + y[t−1]) > x[i]b^2 + x[i−1]b + x[i−2]
-    while(compare(QY, 0, maxQY, X, i-2, i+1) > 0){
+    while(compare(QY, 0, 3, X, i-2, i+1) > 0){
 
       //q[i−t−1] ← q[i−t−1] − 1
-      maxQY = mul(Y, t-1, 2, --Q[i-t-1], QY, base);
+      --Q[iMt];
+      maxQY = BasicSubtractionMethod(QY, 0, maxQY, Y, t-1, t+1, base);
     }
+    */
 
-    //pre 3.3 q[i−t−1] * yb^(i−t−1)
-    maxQY = mul(Y, 0, maxY, Q[i-t-1], QY, base);
-
-    //3.3 x ← x − q[i−t−1]yb^(i−t−1)
+    //3.3 x ← x − q[i−t−1] * yb^(i−t−1)
     //3.4 If x < 0 then set x ← x + yb^(i−t−1) and q[i−t−1] ← q[i−t−1] − 1
-    if(compare(QY, 0, maxQY, X, i-t-1, maxX) > 0){
-      --Q[i-t-1];
+    let maxQY: number = mul(Y, 0, maxY, Q[iMt], QY, base);
+    if(compare(QY, 0, maxQY, X, iMt, maxX) > 0){
+      --Q[iMt];
       maxQY = BasicSubtractionMethod(QY, 0, maxQY, Y, 0, maxY, base);
     }
-    maxX = BasicSubtractionMethod(X, i-t-1, maxX, QY, 0, maxQY, base);
+    for(maxX = BasicSubtractionMethod(X, iMt, maxX, QY, 0, maxQY, base); maxX > 0 && X[maxX - 1] === 0; --maxX){
+      --i;
+    }
   }
 
   //4. r ← x
   //5. Return(q,r)
-  return [Q, X, (Q[nMt] === 0) ? nMt - 1 : nMt, maxX];
+  return [Q, X, (Q[nMt] === 0) ? nMt : nMt + 1, maxX];
 }
 
 /*
-function div(A: number[], lenA: number, divisor: number, base: number): number {
-
-  //Check if dividing by 1
-  if(divisor === 1){
-    return lenA;
-  }
-
-  //Divide dividend by divisor
-  for(let a: number = lenA; a > 0;){
-    A[a-1] = A[a-1] + (A[a] % base) * base;
-    A[a] = 0 | (A[a] / divisor);
-  }
-
-  //Return length
-  return (A[lenA-1] === 0) ? lenA - 1 : lenA;
-}
+  See: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.47.565&rep=rep1&type=pdf
 */
+function divThreeHalvesByTwo(a1: number, a2: number, a3: number, b1: number, b2: number, base: number): number {
+  let q: number = a1*base + a2;
+  let r: number = q % b1;
+  q = 0 | (q / b1);
+  r = r*base - q*b2 + a3;
+  if(r < 0){
+    r = r + b1*base + b2;
+    q = (r < 0) ? q - 2 : q - 1;
+  }
+  return q;
+}
 
 function mul(A: number[], minA: number, lenA: number, multiplier: number, B: number[], base: number): number {
 
--- test/div/basicDivisionMethod.ts
+++ test/div/basicDivisionMethod.ts
@@ -12,7 +12,7 @@ function test(A: number[], B: number[], C: number[], D: number[], base: number):
   //Divide
   [Q, R, maxQ, maxR] = BasicDivisionMethod(A, A.length, B, B.length, base);
 
-  console.log("Q: ", print(Q, 0, Q.length), "R: ", print(R, 0, R.length));
+  console.log("Q: ", print(Q, 0, 0, maxQ, Q.length),"R: ", print(R, 0, 0, maxR, R.length));
 
   //Confirm B was not changed
   assert.equal(B.length, BB.length);
@@ -30,6 +30,83 @@ function test(A: number[], B: number[], C: number[], D: number[], base: number):
 }
 
 describe('BasicDivisionMethod', function(){
+
+  it('should return 10 / 10 in base 10',
+    () => test(
+      [0,1], [0,1],
+      [1], [], 10
+    )
+  );
+
+  it('should return 99 / 99 in base 10',
+    () => test(
+      [9, 9], [9,9],
+      [1], [], 10
+    )
+  );
+
+  it('should return 100 / 10 in base 10',
+    () => test(
+      [0,0,1], [0,1],
+      [0,1], [], 10
+    )
+  );
+
+  it('should return 999 / 11 in base 10',
+    () => test(
+      [9,9,9], [1,1],
+      [0,9], [9], 10
+    )
+  );
+
+  it('should return 999,999,999 / 123 in base 10',
+    () => test(
+      [9,9,9,9,9,9,9,9,9], [3,2,1],
+      [1,8,0,0,3,1,8], [6,3], 10
+    )
+  );
+
+  it('should return 999,999,999 / 321 in base 10',
+    () => test(
+      [9,9,9,9,9,9,9,9,9], [1,2,3],
+      [4,6,2,5,1,1,3], [5,5,2], 10
+    )
+  );
+
+  it('should return 961,748,941 / 15,485,863 in base 2',
+    () => test(
+      [1,0,1,1,0,0,1,1,1,1,1,1,1,0,0,0,1,1,0,0,1,0,1,0,1,0,0,1,1,1], [1,1,1,0,0,1,0,1,1,1,0,1,0,0,1,0,0,0,1,1,0,1,1,1],
+      [0,1,1,1,1,1], [1,1,0,1,1,0,1,0,1,0,1,1,0,0,1,1,0,0,0,1,1], 2
+    )
+  );
+  
+  it('should return 961,748,941 / 15,485,863 in base 3',
+    () => test(
+      [1,1,0,2,1,1,0,2,2,0,0,2,0,0,0,1,1,1,2], [1,1,1,1,2,1,2,0,2,0,1,0,2,0,0,1],
+      [2,2,0,2], [2,2,0,0,0,2,0,2,1,1,0,0,0,1], 3
+    )
+  );
+
+  it('should return 961,748,941 / 15,485,863 in base 7',
+    () => test(
+      [6,2,5,5,0,5,5,5,5,2,3], [1,0,2,5,2,4,5,4,2],
+      [6,1,1], [0,1,6,6,4,5,6,1], 7
+    )
+  );
+
+  it('should return 1,214,015,305,151,164,506,623,822,532,690 / 123,000,456,000,789 in base 10',
+    () => test(
+      [0,9,6,2,3,5,2,2,8,3,2,6,6,0,5,4,6,1,1,5,1,5,0,3,5,1,0,4,1,2,1], [9,8,7,0,0,0,6,5,4,0,0,0,3,2,1],
+      [0,1,2,3,0,0,0,4,5,6,0,0,0,7,8,9], [], 10
+    )
+  );
+
+  it('should return 961,748,941 / 15,485,863 in base 10',
+    () => test(
+      [1,4,9,8,4,7,1,6,9], [3,6,8,5,8,4,5,1],
+      [2,6], [5,3,4,5,2,6,1], 10
+    )
+  );
 
   it('should return 721,948,327 / 84,461 in base 10',
     () => test(
