41d520341ea45a736773080842c3e951d5215f2f
Michael Rojas <mikerojas@google.com>
2017-01-24 17:27:28 -0500

    Fix array regrowth logic in functional pow

--- src/functional/pow.ts
+++ src/functional/pow.ts
@@ -5,35 +5,35 @@ import {growArray} from '../util/arrayUtils';
 import {setOne, setZero, tryMutable} from '../util/intUtils';
 
 export function pow(A: Integer, B: Integer, isMutable?: boolean): Integer {
-  const C: Integer = tryMutable(A, isMutable);
+  A = tryMutable(A, isMutable);
 
   //C^0 = 1
   if(B.precision === 0){
-    return setOne(C);
+    return setOne(A);
   }
 
   //If power is negative
   if(B.isNegative){
 
     //If 1 / 0
-    if(C.precision === 0){
+    if(A.precision === 0){
       throw new EvalError("Divide by zero");
     }
 
-    return setZero(C);
+    return setZero(A);
   }
 
   //0^B = zero
-  if(C.precision === 0){
-    return C;
+  if(A.precision === 0){
+    return A;
   }
 
   //If negative base and even power
-  C.isNegative = C.isNegative && !isEven(B);
+  A.isNegative = A.isNegative && !isEven(B);
 
   //1^B = 1
-  if(C.precision === 1 && C.digits[0] === 1){
-    return C;
+  if(A.precision === 1 && A.digits[0] === 1){
+    return A;
   }
 
   //Set new size
@@ -47,9 +47,9 @@ export function pow(A: Integer, B: Integer, isMutable?: boolean): Integer {
   );
 
   //C = C^B
-  C.precision = exponentiation(
-    C.digits, 0, C.precision, C.base, B.digits.slice(0,B.precision), 0, B.precision, B.base
+  A.precision = exponentiation(
+    A.digits, 0, A.precision, A.base, B.digits.slice(0,B.precision), 0, B.precision, B.base
   );
 
-  return C;
+  return A;
 }
