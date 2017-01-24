703275a6d2640bd02fcbc8b8cd2c467fb0c209f1
Michael Rojas <mikerojas@google.com>
2017-01-24 09:08:10 -0500

    Add tests for functional toString, Refactor functional toString

--- src/functional/toString.ts
+++ src/functional/toString.ts
@@ -2,27 +2,29 @@ import {Globals} from '../globals';
 import {Integer} from '../integer';
 
 //Assumes input cipher is adequate for base
-export function toString(A: Integer, sep: string = null, cipher: ReadonlyArray<string> = null): string {
+export function toString(A: Integer): string {
+  const cipher = Globals.DEFAULT_CIPHER;
+
+  //If zero
+  if(A.precision === 0){
+    return (cipher.length < A.base) ? '0' : cipher[0];
+  }
 
   //If negative
   const sign: string = A.isNegative ? "-" : "";
 
-  //If no custom cipher
-  if(cipher == null){
-
-    //If default cipher too small for base
-    if(Globals.DEFAULT_CIPHER.length < A.base){
-
-      //Print without cipher
-      sep = (sep == null) ? ":": sep;
-      return sign + A.digits.slice(0,A.precision).reverse().join(sep);
-    }
-
-    //Use default cipher
-    cipher = Globals.DEFAULT_CIPHER;
+  //Print without cipher if cipher too small for base
+  if(cipher.length < A.base){
+    return sign + A.digits.slice(0, A.precision).reverse().join(':');
   }
 
   //Print with cipher
-  sep = (sep == null) ? "" : sep;
-  return sign + A.digits.map(v => cipher[v]).reverse().join(sep);
+  let i: number = 0;
+  let n: number = A.precision;
+  const digits = A.digits;
+  const strings = new Array(n);
+  while(n > 0){
+    strings[i++] = cipher[digits[--n]];
+  }
+  return sign + strings.join('');
 }
---
+++ test/functional/toString.ts
@@ -0,0 +1,78 @@
+import {assert} from 'chai';
+import {Globals} from '../../src/globals';
+import {Integer} from '../../src/integer';
+import {toString} from '../../src/functional/toString';
+import {toInteger} from '../../src/util/intUtils';
+
+describe('toString', function(){
+
+  it('should return 0 in small base', function(){
+    const A: Integer = toInteger([], 0, false, 10);
+    const expected: string = '0';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return 0 in large base', function(){
+    const A: Integer = toInteger([], 0, false, Globals.DEFAULT_CIPHER.length + 1);
+    const expected: string = Globals.DEFAULT_CIPHER[0];
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return 1 in small base', function(){
+    const A: Integer = toInteger([1], 1, false, 10);
+    const expected: string = '1';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return 1 in large base', function(){
+    const A: Integer = toInteger([1], 1, false, Globals.DEFAULT_CIPHER.length + 1);
+    const expected: string = Globals.DEFAULT_CIPHER[1];
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return -1 in small base', function(){
+    const A: Integer = toInteger([1], 1, true, 10);
+    const expected: string = '-1';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return -1 in large base', function(){
+    const A: Integer = toInteger([1], 1, true, Globals.DEFAULT_CIPHER.length + 1);
+    const expected: string = '-' + Globals.DEFAULT_CIPHER[1];
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return home in base 128', function(){
+    const A: Integer = toInteger([1,0,0,127], 4, false, 128);
+    const expected: string = '127:0:0:1';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return home in base 10', function(){
+    const A: Integer = toInteger([1,0,0,7,2,1], 6, false, 10);
+    const expected: string = '127001';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return -home in base 128', function(){
+    const A: Integer = toInteger([1,0,0,127], 4, true, 128);
+    const expected: string = '-127:0:0:1';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+
+  it('should return -home in base 10', function(){
+    const A: Integer = toInteger([1,0,0,7,2,1], 6, true, 10);
+    const expected: string = '-127001';
+    const actual: string = toString(A);
+    assert.equal(expected, actual);
+  });
+});
